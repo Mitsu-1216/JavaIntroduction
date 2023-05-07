8cd81cdf52ca96dbc3a7c216410257ec88a9fe34
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-07 14:10:30 +0900

    13_1

---
+++ src/practice13/practice13/Hero.java
@@ -0,0 +1,23 @@
+package practice13.practice13;
+
+public class Hero {
+    private int hp;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+}
---
+++ src/practice13/practice13/Wand.java
@@ -0,0 +1,22 @@
+package practice13.practice13;
+
+public class Wand {
+    private String name;
+    private double power;
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
