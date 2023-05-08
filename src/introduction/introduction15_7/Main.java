a6ae389ff0defb5baf77f1c85d9146f4df7d8a9c
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-09 07:55:33 +0900

    15_7

---
+++ src/introduction/introduction15_7/Main.java
@@ -0,0 +1,12 @@
+package introduction.introduction15_7;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "abc,def:ghi";
+        // 1つの文字列を複数に分割
+        String[] words = s.split("[,:]");
+        for (String w : words) {
+            System.out.print(w + "->");
+        }
+    }
+}
