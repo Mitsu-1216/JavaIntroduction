3329f0dea01a9210e5ccbbbd8efd2c3224ee8ec5
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-09 07:58:58 +0900

    15_8

---
+++ src/introduction/introduction15_8/Main.java
@@ -0,0 +1,11 @@
+package introduction.introduction15_8;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "abc,def:ghi";
+        // 1つの文字列を複数に分割
+        // behをXに置き換える
+        String w = s.replaceAll("[beh]", "X");
+        System.out.println(w);
+    }
+}
