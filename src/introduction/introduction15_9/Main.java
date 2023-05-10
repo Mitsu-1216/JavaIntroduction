a994269b3f0605d4c70e76c2c63b75ec6aafcf51
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-10 22:42:37 +0900

    15_9

--- src/introduction/introduction15_9/Main.java
+++ src/introduction/introduction15_9/Main.java
@@ -3,9 +3,9 @@ package introduction.introduction15_9;
 public class Main {
     public static void main(String[] args) {
         Hero hero = new Hero();
-        hero.name = "minato";
-        hero.job = "hero";
-        hero.gold = 280;
+        hero.setName("minato");
+        hero.setJob("hero");
+        hero.setGold(280);
 
         final String FORMAT = "%-9s %-13s 所持金%,6d";
         String s = String.format(FORMAT, hero.getName(), hero.getJob(), hero.getGold());
