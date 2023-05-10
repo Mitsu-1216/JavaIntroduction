d092d4b4ea017ed79f227804f64103cb0765d170
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-10 22:43:49 +0900

    15_9

--- src/introduction/introduction15_9/Main.java
+++ src/introduction/introduction15_9/Main.java
@@ -10,5 +10,7 @@ public class Main {
         final String FORMAT = "%-9s %-13s 所持金%,6d";
         String s = String.format(FORMAT, hero.getName(), hero.getJob(), hero.getGold());
         System.out.println(s);
+        System.out.printf("製品番号%s-%02d", "SJV", 3);
+
     }
 }
