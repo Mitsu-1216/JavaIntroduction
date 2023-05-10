ceb5012c99123ef44696f5a30b464afbc59a68bc
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-11 07:00:12 +0900

    15_10

---
+++ src/introduction/introduction15_10/Main.java
@@ -0,0 +1,14 @@
+package introduction.introduction15_10;
+
+public class Main {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        // 時間がかかる処理を記述
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("処理にかかった時間は" + (end - start) + "ミリ秒です！");
+
+    }
+}
