ba0dcb38cda1ad8fc8f4e89bf1248b2af149ddbe
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-12 20:49:15 +0900

    9_5

--- src/practice6/introduction9_5/Main.java
+++ src/practice6/introduction9_5/Main.java
@@ -4,15 +4,15 @@ public interface Main {
     public static void main(String[] args) {
 
         Hero h1 = new Hero();
-        h1.name = "ミナト";
+        h1.name = "デク";
         h1.hp = 100;
 
         Hero h2 = new Hero();
-        h2.name = "アサカ";
+        h2.name = "大爆殺王";
         h2.hp = 100;
 
         Wizard w = new Wizard();
-        w.name = "スガワラ";
+        w.name = "ショート";
         w.hp = 50;
 
         w.heal(h1);
