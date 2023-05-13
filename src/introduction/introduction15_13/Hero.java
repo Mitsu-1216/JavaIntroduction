2cd3f6036798d4aeee86e3b4f0b732587c55e740
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-13 12:17:36 +0900

    15_13

---
+++ src/introduction/introduction15_13/Hero.java
@@ -0,0 +1,32 @@
+package introduction.introduction15_13;
+
+public class Hero {
+    private String name;
+    private String job;
+    private int gold;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+}
---
+++ src/introduction/introduction15_13/Main.java
@@ -0,0 +1,17 @@
+package introduction.introduction15_13;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+        public static void main(String[] args) throws Exception {
+                SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                // 文字列からDateインスタンスを生成
+                Date d = f.parse("2023/05/13 12:13:45");
+                System.out.println(d);
+                // Dateインスタンスから文字列を生成
+                Date now = new Date();
+                String s = f.format(now);
+                System.out.println("今日は" + s + "です");
+        }
+}
