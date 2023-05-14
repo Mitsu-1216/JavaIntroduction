3ecdb78cea60a639088316399111f7530b4536c7
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-14 21:16:10 +0900

    16_1

---
+++ src/introduction16/introduction16_1/Main.java
@@ -0,0 +1,22 @@
+package introduction16.introduction16_1;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Integer型のインスタンスを保持
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+
+        // リストに追加
+        nums.add(3);
+        nums.add(32);
+        nums.add(324);
+
+        // リストをループ
+        for (int i : nums) {
+            System.out.println(i);
+        }
+
+    }
+}
