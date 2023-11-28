aa406f7b2a60fd257af0f70d717e1614b937db77
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-11-29 08:33:34 +0900

    HaashMap生成

---
+++ src/introduction/introduction16/introduction16_6/Main.java
@@ -0,0 +1,22 @@
+package introduction16.introduction16_6;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // HaashMap生成
+        Map<Strirng,Integer> pref = new HashMap<String,Integer>();
+
+        // リストに追加
+        nums.add(3);
+        nums.add(32);
+        nums.add(324);
+
+        // リストをループ
+        for (int i : nums) {
+            System.out.println(i);
+        }
+
+    }
+}
