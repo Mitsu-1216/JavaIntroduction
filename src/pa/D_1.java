4a5a5fc87ae56c4e3ae07cecc015669655825335
Mitsu-1216 <adieuer._.03.26@icloud.com>
2024-02-03 21:12:55 +0900

    配列の型変換

--- src/pa/D_1.java
+++ src/pa/D_1.java
@@ -1,15 +1,17 @@
 package pa;
 
-import java.util.*;
+import java.util.stream.Stream;
 
 public class D_1 {
     public static void main(String[] args) {
-        String line = "Hxexlxlxox!x!x";
-        String word = "";
-        for(int i = 0; i < line.length()/2; i++){
-            word += String.valueOf(line.charAt(i*2));
-            }
-        System.out.println(word);
+        String num = "1 2 3 4 5";
+        String[] sArray = num.split(" ");
+        
+        // String型配列 を int型配列 に変換
+        int[] iArray = Stream.of(sArray).mapToInt(Integer::parseInt).toArray();
+
+        int count = iArray[2];
+        System.out.println(count * 2);
     }
 }
 
