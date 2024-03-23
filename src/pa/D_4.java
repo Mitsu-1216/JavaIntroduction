9ec32f384c72abf40d32ea1027c8cd49dbf6cfb6
Mitsu-1216 <adieuer._.03.26@icloud.com>
2024-03-23 17:45:34 +0900

    D4

---
+++ src/pa/D_4.java
@@ -0,0 +1,27 @@
+
+
+import java.util.*;
+public class D_4 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String wantFood = sc.nextLine();
+        int count = Integer.parseInt(sc.nextLine());
+        String menu = sc.nextLine();
+        String[] menus = menu.split(" ");
+        int ngcnt= 1;
+        
+        for(int i = 0; i < menus.length; i++){
+            if(wantFood.equals(menus[i])){
+                System.out.println("Yes");
+                break;
+            } else {
+                ngcnt++;
+                if(ngcnt == count){
+                    System.out.println("No");
+                }
+            }
+        }
+    }
+}
